cd098d8be636382e46bff2a62cc917cfcd6cf3a6
Shivrajsantoshdeshmukh <deshmukhshivraj932@gmail.com>
2024-09-22 09:33:19 +0530

    09:30 AM - 22/09/2024 - I this lecture I stared javascript and how to work Event handling

---
+++ LECTURE_11/01_CLASS_CODE/script.js
@@ -0,0 +1,20 @@
+let num = 10;
+var inum; // Declared but not used
+
+var title = document.querySelector("h1");
+var hold = document.querySelector("h2");
+title.textContent = "Ram Ram Bhau"; // Added semicolon
+
+hold.textContent = num; // Added semicolon
+
+
+
+
+
+
+
+
+
+
+
+
